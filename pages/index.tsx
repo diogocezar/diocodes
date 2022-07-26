aec6c2a8b418b39f5098125a1af3811c314d7800
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2022-07-26 20:25:37 -0300

    feat: first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>diocodes</title>
+        <meta name="description" content="Just my contacts hub" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Under Construction</h1>
+    </div>
+  )
+}
+
+export default Home
