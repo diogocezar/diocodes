5877eddbc1049f5efa3e531bb4f042f40286e994
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-02-24 21:38:06 -0300

    feat: includding rating

--- src/app/avaliation/[id]/page.tsx
+++ src/app/avaliation/[id]/page.tsx
@@ -1,20 +1,26 @@
 "use client";
 import { z } from "zod";
-import { Star } from "@phosphor-icons/react";
 import { Container } from "@/components/app/container";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import { Paragraph } from "@/components/app/paragraph";
 import { Header } from "@/components/containers/header";
 import { SchemaAvaliation } from "@/schemas/schema-avaliation";
 import { SubTitle } from "@/components/app/titles";
+import Rating from "@/components/app/rating";
+import { useState } from "react";
+import { Paragraph } from "@/components/app/paragraph";
 
 export default function Avaliation({ params }: { params: { id: string } }) {
+  const [rating, setRating] = useState<number>(1);
+  const handleRatingChange = (newRating: number) => {
+    setRating(newRating);
+  };
   const form = useForm<z.infer<typeof SchemaAvaliation>>({
     resolver: zodResolver(SchemaAvaliation),
   });
+  console.log(params.id);
   async function onSubmit() {}
   return (
     <>
@@ -23,14 +29,8 @@ export default function Avaliation({ params }: { params: { id: string } }) {
         <SubTitle className="mb-8">
           De 1 a 5, qual a sua nota para a mentoria?
         </SubTitle>
-        <div className="flex flex-row gap-2">
-          <Star className="text-pink h-8 w-8 " />
-          <Star className="text-pink h-8 w-8" />
-          <Star className="text-pink h-8 w-8" />
-          <Star className="text-pink h-8 w-8" />
-          <Star className="text-pink h-8 w-8" />
-        </div>
-
+        <Rating onChange={handleRatingChange} />
+        <Paragraph>Você escolheu a nota {rating}</Paragraph>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-1/2">
             <Button type="submit" className="mt-20">
---
+++ src/components/app/rating.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Star } from "@phosphor-icons/react";
+
+interface StarIconProps {
+  filled: boolean;
+  onHover: () => void;
+  onClick: () => void;
+}
+
+const StarIcon: React.FC<StarIconProps> = ({ filled, onHover, onClick }) => {
+  return (
+    <div className="cursor-pointer" onMouseEnter={onHover} onClick={onClick}>
+      {filled ? (
+        <Star weight="fill" className="text-green" size={36} />
+      ) : (
+        <Star className="text-foreground" size={36} />
+      )}
+    </div>
+  );
+};
+
+interface RatingProps {
+  onChange: (newRating: number) => void;
+}
+
+const Rating: React.FC<RatingProps> = ({ onChange }) => {
+  const [rating, setRating] = useState<number>(1);
+
+  const handleRatingChange = (newRating: number) => {
+    setRating(newRating);
+    onChange(newRating);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <div className="mb-8 flex flex-row items-center gap-1">
+        {[1, 2, 3, 4, 5].map((index) => (
+          <StarIcon
+            key={index}
+            filled={index <= rating}
+            onHover={() => handleRatingChange(index)}
+            onClick={() => handleRatingChange(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
