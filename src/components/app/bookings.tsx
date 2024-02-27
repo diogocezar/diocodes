6ece1c5002b68dc48f41c8d064a2ff2f3cc9302b
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-02-27 10:28:33 -0300

    feat: layout card main change

--- src/components/app/bookings.tsx
+++ src/components/app/bookings.tsx
@@ -7,9 +7,9 @@ const Bookings = ({ booking }: any) => {
       className={`shadow- w-full cursor-crosshair md:w-[50%] lg:w-[33.3%] xl:w-[25%] ${booking.isActive ? "opacity-100" : "opacity-20"}`}
     >
       <div className="group">
-        <div className="bg-comment group-hover:bg-purple m-2 gap-2 p-6 transition duration-300 ease-in-out">
+        <div className="bg-card group-hover:bg-background-dark m-2 gap-2 p-6 transition duration-300 ease-in-out">
           <div className="text-foreground font-poppins mb-2 text-sm font-bold">
-            <Calendar size={32} className="text-background mb-3" />{" "}
+            <Calendar size={32} className="text-green mb-3" />{" "}
             {booking.attendee.toUpperCase()}
           </div>
           <div className="text-pink group-hover:text-foreground text-xs font-medium transition duration-300 ease-in-out">
