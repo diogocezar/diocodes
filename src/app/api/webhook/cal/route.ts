d9f228bc3d784a797559772ee1fbe3994718d78c
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-03-30 08:15:59 -0300

    feat: includding cal webhook

--- src/app/api/webhook/cal/route.ts
+++ src/app/api/webhook/cal/route.ts
@@ -8,18 +8,22 @@ export const POST = async (req: Request) => {
   try {
     const secret = process.env.WEBHOOK_CAL_SECRET;
     const signature = req.headers.get("X-Cal-Signature-256");
-    if (!secret || !signature) throw new Error("Missing secret or signature");
     const payload = await req.json();
-    const hmac = crypto.createHmac("sha256", secret);
-    hmac.update(payload);
-    const hmacValue = hmac.digest().toString();
-    console.log({ secret, signature, hmacValue });
-    if (signature !== hmacValue) {
+    if (!secret || !signature || !payload)
+      throw new Error("Missing secret, payload or signature.");
+    var hmacDigest = crypto
+      .createHmac("sha1", secret)
+      .update(JSON.stringify(payload))
+      .digest("hex");
+    console.log(hmacDigest);
+    console.log({ secret, signature, hmacDigest });
+    if (signature !== hmacDigest) {
       return new Response("Unauthorized", { status: 401 });
     }
     const result = await sync({ debug: true });
     return new Response(JSON.stringify({ result }), { status: 200 });
   } catch (error) {
+    console.log(error);
     return new Response(JSON.stringify({ error }), { status: 500 });
   }
 };
