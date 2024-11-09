f88d86e37f325c39e14d1d04b234ec2ed522d30b
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2024-11-08 21:57:45 -0300

    feat: favicon and ogimage

--- src/app/(main)/(main)/layout.tsx
+++ src/app/(main)/(main)/layout.tsx
@@ -58,9 +58,9 @@ export const metadata: Metadata = {
       "Descubra o mundo da tecnologia com Diogão! Mentorias gratuitas e planos pagos, recursos de aprendizado, carreira e orientação em programação.",
     images: [
       {
-        url: "https://diocodes.dev/og.png",
-        width: 280,
-        height: 280,
+        url: "https://diocodes.dev/assets/images/og/og.png",
+        width: 1200,
+        height: 630,
         alt: "Mentorias com o Diogão",
       },
     ],
@@ -87,6 +87,23 @@ export default async function RootLayout({
     <html lang="pt-BR">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/zbi8mzg.css" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="assets/images/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="assets/images/favicon/favicon.svg"
+        />
+        <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="assets/images/favicon/apple-touch-icon.png"
+        />
       </head>
       <body className={cn("bg-dots antialiased font-obviously")}>
         <CustomSessionProvider session={session}>
